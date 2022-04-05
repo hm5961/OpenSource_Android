6947ca10b4a4af279c6032bed00ae98b7625e7fa
hm5961 <71277818+hm5961@users.noreply.github.com>
2022-04-05 18:24:35 +0900

    5-2

--- 220406/Project5_3/app/src/main/java/com/khm/project5_3/MainActivity.java
+++ 220406/Project5_3/app/src/main/java/com/khm/project5_3/MainActivity.java
@@ -2,15 +2,50 @@ package com.khm.project5_3;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Color;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    private EditText edit;
+    private Button btn;
+    private TextView text;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        getSupportActionBar().setDisplayShowHomeEnabled(true);
-        getSupportActionBar().setIcon(R.drawable.icon_android);
+        //setContentView(R.layout.activity_main);
+
+        LinearLayout.LayoutParams params = new LinearLayout.LayoutParams(
+                LinearLayout.LayoutParams.MATCH_PARENT,
+                LinearLayout.LayoutParams.MATCH_PARENT);
+
+        LinearLayout baseLayout = new LinearLayout(this);
+        baseLayout.setOrientation(LinearLayout.VERTICAL);
+        setContentView(baseLayout,params);
+
+        edit = new EditText(this);
+        baseLayout.addView(edit);
+
+        btn = new Button(this);
+        btn.setText("버튼입니다");
+        btn.setBackgroundColor(Color.YELLOW);
+        baseLayout.addView(btn);
+
+        text = new TextView(this);
+        text.setTextSize(20);
+        text.setTextColor(Color.MAGENTA;
+        baseLayout.addView(text);
+
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                text.setText(edit.getText().toString());
+            }
+        });
     }
 }
