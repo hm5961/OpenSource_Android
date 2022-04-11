73339418b5d861bc50017cd3634b5f60adb6fee3
hm5961 <71277818+hm5961@users.noreply.github.com>
2022-04-11 23:49:09 +0900

    make

---
+++ 220413/project_06_01/app/src/main/java/com/khm/project_06_01/MainActivity.java
@@ -0,0 +1,14 @@
+package com.khm.project_06_01;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
