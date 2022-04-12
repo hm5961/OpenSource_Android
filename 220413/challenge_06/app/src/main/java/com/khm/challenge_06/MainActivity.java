80c4c36cb0d342381f24154f27870a9fd1d57319
hm5961 <71277818+hm5961@users.noreply.github.com>
2022-04-12 20:18:13 +0900

    ch06

--- 220413/challenge_06/app/src/main/java/com/khm/challenge_06/MainActivity.java
+++ 220413/challenge_06/app/src/main/java/com/khm/challenge_06/MainActivity.java
@@ -3,6 +3,9 @@ package com.khm.challenge_06;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.SeekBar;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,27 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        getSupportActionBar().setDisplayShowHomeEnabled(true);
+        getSupportActionBar().setIcon(R.drawable.icon_android);
+
+        int number;
+
+        final ProgressBar progressBar = findViewById(R.id.progressBar);
+        final EditText editText = findViewById(R.id.editText);
+
+        SeekBar seekBar = (SeekBar) findViewById(R.id.seekBar);
+        seekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener(){
+            public void onProgressChanged(SeekBar seekBar, int progress,
+                                          boolean fromUser) {
+                progressBar.setProgress(progress);
+                editText.setText(new StringBuilder().append(progress));
+            }
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+            }
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+            }
+        });
     }
 }
