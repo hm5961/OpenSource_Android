26f3610f7b783d01bb3f8a065ee7fbc4aee77e6f
hm5961 <71277818+hm5961@users.noreply.github.com>
2022-05-04 18:44:56 +0900

    220504

---
+++ 220504/Project10_1/app/src/main/java/com/example/project10_1/SecondActivity.java
@@ -0,0 +1,22 @@
+package com.example.project10_1;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class SecondActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.second);
+
+        Button btnReturn = (Button)findViewById(R.id.btnReturn);
+        btnReturn.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View v){
+                finish();
+            }
+        });
+    }
+}
